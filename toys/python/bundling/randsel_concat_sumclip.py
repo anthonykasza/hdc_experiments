95bfa50ef9ee60b7bae1b339ee5e008adb2ca8ee
ak <a@k>
2026-01-08 17:19:15 -0500

    fixup bundle comparisons

--- toys/python/bundling/randsel_concat_sumclip.py
+++ toys/python/bundling/randsel_concat_sumclip.py
@@ -1,4 +1,4 @@
-# Randsel and Concat are the same thing.
+# Randsel and Concat are the same thing. They enable weighting.
 # Neither requires clipping nor normalization.
 # Both give the same results regardless of hv element type/range.
 # Both are less robust than sum+clip. Perhaps they are faster?
@@ -48,8 +48,24 @@ def concat_bundle(*hvs):
 
   return acc
 
-NUM_TRIALS = 100
-NUM_SIGNALS = 20
+
+def randsel_weighted_bundle(weights, *hvs):
+  acc = hdv(n=len(hvs[0]), all=0)
+  n = len(hvs)
+  for i in range(len(acc)):
+    idx = int(np.random.choice(n, p=weights))
+    hv = hvs[idx]
+    acc[i] = hv[i]
+  return acc
+
+def normalize_weights(w):
+  return [each / sum(w) for each in w]
+
+
+NUM_TRIALS = 25
+NUM_SIGNALS = 5
+# len(WEIGHTS) must equal NUM_SIGNALS
+WEIGHTS = normalize_weights([0, 0, 0, 0, 1,])
 
 rb_results = []
 cb_results = []
@@ -61,6 +77,7 @@ cb_noise = []
 sb_noise = []
 wb_noise = []
 
+
 for i in range(NUM_TRIALS):
   signals = [hdv() for i in range(NUM_SIGNALS)]
   noise = hdv()
@@ -68,30 +85,44 @@ for i in range(NUM_TRIALS):
   rb = randsel_bundle(*signals)
   cb = concat_bundle(*signals)
   sb = sumclip_bundle(*signals)
+  wb = randsel_weighted_bundle(WEIGHTS, *signals)
 
   rb_results.append([sim(signal, rb) for signal in signals])
   cb_results.append([sim(signal, cb) for signal in signals])
   sb_results.append([sim(signal, sb) for signal in signals])
+  wb_results.append([sim(signal, wb) for signal in signals])
 
   rb_noise.append(sim(noise, rb))
   cb_noise.append(sim(noise, cb))
   sb_noise.append(sim(noise, sb))
+  wb_noise.append(sim(noise, wb))
 
 
 print("Ave sim of bundle to constituents: Randsel")
-for idx in range(NUM_SIGNALS):
-  print('  ', idx, np.mean([trial for trial in rb_results]))
-print('  noise', np.mean(np.mean(rb_noise)))
+for s_idx in range(NUM_SIGNALS):
+  x = np.mean([rb_results[trial_idx][s_idx] for trial_idx in range(NUM_TRIALS)])
+  print('  signal', s_idx, x)
+print('  noise', np.mean(rb_noise))
 print()
 
 print("Ave sim of bundle to constituents: Concatenation")
-for idx in range(NUM_SIGNALS):
-  print('  ', idx, np.mean([trial for trial in cb_results]))
-print('  noise', np.mean(np.mean(cb_noise)))
+for s_idx in range(NUM_SIGNALS):
+  x = np.mean([cb_results[trial_idx][s_idx] for trial_idx in range(NUM_TRIALS)])
+  print('  signal', s_idx, x)
+print('  noise', np.mean(cb_noise))
 print()
 
 print("Ave sim of bundle to constituents: Sum and Clip")
-for idx in range(NUM_SIGNALS):
-  print('  ', idx, np.mean([trial for trial in sb_results]))
-print('  noise', np.mean(np.mean(sb_noise)))
+for s_idx in range(NUM_SIGNALS):
+  x = np.mean([sb_results[trial_idx][s_idx] for trial_idx in range(NUM_TRIALS)])
+  print('  signal', s_idx, x)
+print('  noise', np.mean(sb_noise))
+print()
+
+print("Ave sim of bundle to constituents: Randsel weighted")
+print(WEIGHTS)
+for s_idx in range(NUM_SIGNALS):
+  x = np.mean([wb_results[trial_idx][s_idx] for trial_idx in range(NUM_TRIALS)])
+  print('  signal', s_idx, x)
+print('  noise', np.mean(wb_noise))
 print()
